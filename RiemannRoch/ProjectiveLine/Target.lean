995dff3e77a67285e3d83e3b15f3e7b3324733e7
Steven Sabean <steves1962wmoc@gmail.com>
2026-07-31 08:02:06 -0500

    docs: add source header to target module

--- RiemannRoch/ProjectiveLine/Target.lean
+++ RiemannRoch/ProjectiveLine/Target.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Steven Sabean. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Steven Sabean
+-/
+
 import RiemannRoch.ProjectiveLine.SheavesAndCech
 
 /-!
