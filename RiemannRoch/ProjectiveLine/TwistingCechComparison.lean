e82e9d38634cfbe870daf7645e8da060222e6dce
Steven Sabean <steven@ceadmath.org>
2026-09-26 08:57:03 -0500

    Limit ring-hom extensionality depth

--- RiemannRoch/ProjectiveLine/TwistingCechComparison.lean
+++ RiemannRoch/ProjectiveLine/TwistingCechComparison.lean
@@ -230,7 +230,7 @@ private theorem x1TrivialTopRestriction_ring_coordinates
           ((laurentPolynomialEquivOverlapAway k).symm).toCommRingCatIso.hom =
         ((x1ChartRingEquiv k).symm).toCommRingCatIso.hom ≫
           CommRingCat.ofHom (x1CoordinateRestrictionRingHom k) := by
-    ext z
+    ext z : 1
     change
       (laurentPolynomialEquivOverlapAway k).symm (x1ToOverlapMap k z) =
         (laurentPolynomialEquivOverlapAway k).symm
